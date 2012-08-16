4f61e46d5a2311842fd5ccbcf2643fa2b6d8d148
Andrew Haines <andrew@haines.org.nz>
2012-08-16 01:33:44 +0100

    Initialize RSpec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+require 'bundler/setup'
+require 'font_awesome_rails'
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+
+  config.order = 'random'
+
+  config.mock_with :mocha
+end
